a10330a3e71f8d272888e2e3a09b05246eabff93
romeshshil <romeshshil99@gmail.com>
2018-05-21 01:09:55 +0600

    angular Route first Commit

---
+++ src/app/configRoute.ts
@@ -0,0 +1,17 @@
+import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+
+export const appRoutes: Routes = [
+  { path: 'home', 
+    component: HomeComponent 
+  },
+  {
+    path: 'about',
+    component: AboutComponent
+  },
+  { path: 'contact',
+    component: ContactComponent
+  }
+];
